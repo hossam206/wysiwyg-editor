4f95f0a6d87905ebf39db8f607a7234c6a0211bc
hossam206 <jhossam818@gmail.com>
2025-06-13 03:16:56 +0300

    add custom toolbar

--- src/types/WysiwygEditor.ts
+++ src/types/WysiwygEditor.ts
@@ -1,6 +1,12 @@
+import type { EditorState } from "draft-js";
+
 export type WysiwygEditorProps = {
-  value?: any; // content
-  onChange?: (content: any) => void;
+  value?: any;
+  onChange?: (val: any) => void;
   className?: string;
   style?: React.CSSProperties;
+  renderToolbar?: (options: {
+    currentStyle: ReturnType<EditorState["getCurrentInlineStyle"]>;
+    toggleStyle: (style: string) => void;
+  }) => React.ReactNode;
 };
